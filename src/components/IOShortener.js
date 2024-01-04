b5f8b3eff8126595d97d8270b2d6f0c5e9a4757a
CodeWithAsutosh <asutoshsahu98@gmail.com>
2024-01-05 01:24:03 +0530

    URL Short Front V0.0.1

--- src/components/IOShortener.js
+++ src/components/IOShortener.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import baseUrl from './../config.js';
+import Qrcode from "react-qr-code";
 
 const IOShortener = () => {
-  const [shortenLink, setShortenLink] = useState("Short URL");
+  const [shortenLink, setShortenLink] = useState("");
   const [copied, setCopied] = useState(false);
   const [longUrl, setLongUrl] = useState("");
 
-  const handleClick = async() => {
+  const handleClick = async () => {
     console.log(baseUrl);
     let result = await fetch(`${baseUrl}/generateShortUrl`, {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ longUrl: longUrl })
     })
-    result=await result.json()
+    result = await result.json()
     console.log(result);
 
-    if(result.status===false) {
+    if (result.status === false) {
       alert(result.message);
     } else {
       setShortenLink(result.data.shortUrl);
@@ -49,12 +50,18 @@ const IOShortener = () => {
 
       <>
         {shortenLink && (
-          <div className="result">
-            <p>{shortenLink}</p>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+            <div className="result">
+              <p>{shortenLink}</p>
 
-            <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
-              <button className={copied ? "copied" : ""}>Copy</button>
-            </CopyToClipboard>
+              <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
+                <button className={copied ? "copied" : ""}>Copy</button>
+              </CopyToClipboard>
+
+            </div>
+            <div className="qr">
+              <Qrcode value={shortenLink} size="200" />
+            </div>
           </div>
         )}
       </>
